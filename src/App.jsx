2befd6f333ec6d46090e5883c5641303fcbd73d1
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-06-27 09:59:28 +0200

    added drop confetti and downloaded packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Die from "./components/Die"
 import { nanoid } from "nanoid"
+import DropConfetti from './components/DropConfetti'
 
 
 const createAllDice = () => { //create an array of objects
-    return Array.from({length: 10}, () => (
+    return Array.from({length: 2}, () => (
         {
             value: Math.ceil(Math.random() * 6), 
             isHeld: false, 
@@ -17,7 +18,7 @@ console.log("id",createAllDice())
 
 function App() {
     const [dice, setdice] = React.useState(createAllDice)
-
+    
     const toggleHold = (id) => { //loop through the array of objects from state, if the clicked index is the same as index of the object in state, toggle its held property
         setdice(prevState => prevState.map((oneDie) => {
             return id ===  oneDie.id ? {...oneDie, isHeld: !oneDie.isHeld} : oneDie
@@ -26,20 +27,23 @@ function App() {
     
     const rollDice = () => {   //loop through the array of objects from state and check each .isHeld and toggle the one clicked object's .isHeld
         setdice(prevValue => prevValue.map(oneDie => { //if you want to skip curlies, remove return too
-           return !oneDie.isHeld ? {...oneDie, value: Math.ceil(Math.random() * 6)} :  oneDie
+            return !oneDie.isHeld ? {...oneDie, value: Math.ceil(Math.random() * 6)} :  oneDie
         }))
     }
-
+    
     let gameWon = dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)
+    let btnName = gameWon ? "New game" : "Roll"
+
     const startNewGame = () => { //when the last die is clicked to be held, swap all back to isHeld: false
-        if(gameWon){
-            setdice(dice.map(oneDie => ({...oneDie, isHeld: false})))
+        if(btnName === "New game"){
+            // setdice(dice.map(oneDie => ({...oneDie, isHeld: false})))
+            console.log("game won")
         }
     }
     
     startNewGame()
 
-    let btnName = startNewGame
+    
 
     const allDice = dice.map((oneDie, index) => { //map through the state array and extract data - the properties + key + add props function
         return (<Die key={index} value={oneDie.value} isHeld={oneDie.isHeld} toggleHold={() => toggleHold(oneDie.id)}/>) //id comes from dice
@@ -49,12 +53,13 @@ function App() {
     <>
     <main>
         <div className="big-box">
+            {gameWon && <DropConfetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-section">
                 {allDice}
             </div>
-            <button onClick={rollDice} className="roll-dice">{!gameWon ? "New game" : "Roll"}</button>
+            <button onClick={rollDice} className="roll-dice">{btnName}</button>
         </div>
     </main>
     </>
